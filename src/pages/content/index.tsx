e4f19e8a84d5c4af805377514c64ae9f8f9d2676
Mauricio Blum <mauriciocblum@gmail.com>
2023-03-21 18:14:12 +0100

    (feat): Twitter test

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,32 +1,23 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
 function Content(): JSX.Element {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <p>
-          Edit <code>src/pages/content/index.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Content styled</p>
-      </header>
+    <div className="bg-blue-500 w-full">
+      <p>Test Twitter New Thing!</p>
     </div>
   );
 }
 
 function init() {
   const rootContainer = document.body;
-  if (!rootContainer) throw new Error("Can't find Content root element");
-  const root = createRoot(rootContainer);
-  root.render(<Content />);
+  setTimeout(() => {
+    const twitterDiv = document.body.querySelector('div.css-1dbjc4n') as HTMLDivElement;
+    if (!rootContainer) throw new Error("Can't find Content root element");
+    if (!twitterDiv) throw new Error("Can't find Twiiter DIV element");
+    const root = createRoot(twitterDiv);
+    root.render(<Content />);
+  }, 10000);
 }
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener('DOMContentLoaded', init);
